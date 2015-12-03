a9e7f54f1897e913ea37030672e154ae941577dc
chriskohlbrenner <c.kohlbrenner@gmail.com>
2015-12-03 15:34:35 -0500

    complete voting logic

--- voting-server/src/core.js
+++ voting-server/src/core.js
@@ -4,12 +4,30 @@ export function setEntries(state, entries) {
   return state.set('entries', List(entries));
 }
 
+function getWinners(vote) {
+  if (!vote) return List();
+  const a = vote.getIn(['pair', 0])
+  const b = vote.getIn(['pair', 1])
+  const aVotes = vote.getIn(['tally', a], 0);
+  const bVotes = vote.getIn(['tally', b], 0);
+  if      (aVotes > bVotes)  return List([a]);
+  else if (aVotes < bVotes)  return List([b]);
+  else                       return List([a, b]);
+}
+
+
 export function next(state) {
-  const entries = state.get('entries');
-  return state.merge({
-    vote: Map({pair: entries.take(2)}),
-    entries: entries.skip(2)
-  });
+  const entries = state.get('entries').concat(getWinners(state.get('vote')));
+  if (entries.size === 1) {
+    return state.remove('vote')
+                .remove('entries')
+                .set('winner', entries.first());
+  } else {
+    return state.merge({
+      vote: Map({pair: entries.take(2)}),
+      entries: entries.skip(2)
+    });
+  }
 }
 
 export function vote(state, entry) {
