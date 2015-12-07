02b1c5c69c9f213bb966ac9f4af8cc817b2a43b3
chriskohlbrenner <c.kohlbrenner@gmail.com>
2015-12-06 20:37:47 -0500

    implement reducer, connect client to redux

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,14 +1,7 @@
 import React from 'react';
-import {List, Map} from 'immutable';
-
-const pair = List.of('Jordan', 'Costa Rica');
-const tally = Map({'Jordan':5, 'Costa Rica':3});
 
 export default React.createClass({
   render: function() {
-    return React.cloneElement(this.props.children, {
-      pair: pair,
-      tally: tally
-    });
+    return this.props.children;
   }
 });
---
+++ client/test/reducer_spec.js
@@ -0,0 +1,70 @@
+import {Map, fromJS} from 'immutable';
+import {expect} from 'chai';
+
+import reducer from '../src/reducer';
+
+describe('reducer', () => {
+
+  it('handles SET_STATE', () => {
+    const initialState = Map();
+    const action = {
+      type: 'SET_STATE',
+      state: fromJS({
+        vote: {
+          pair: ['Jordan', 'Costa Rica'],
+          tally: {'Jordan': 1}
+        }
+      })
+    };
+    const nextState = reducer(initialState, action);
+
+    expect(nextState).to.equal(fromJS({
+      vote: {
+        pair: ['Jordan', 'Costa Rica'],
+        tally: {'Jordan': 1}
+      }
+    }));
+  });
+
+  it('handles SET_STATE with plain JS payload', () => {
+    const initialState = Map();
+    const action = {
+      type: 'SET_STATE',
+      state: {
+        vote: {
+          pair: ['Jordan', 'Costa Rica'],
+          tally: {'Jordan': 1}
+        }
+      }
+    };
+    const nextState = reducer(initialState, action);
+
+    expect(nextState).to.equal(fromJS({
+      vote: {
+        pair: ['Jordan', 'Costa Rica'],
+        tally: {'Jordan': 1}
+      }
+    }));
+  });
+
+  it('handles SET_STATE without initial state', () => {
+    const action = {
+      type: 'SET_STATE',
+      state: {
+        vote: {
+          pair: ['Jordan', 'Costa Rica'],
+          tally: {'Jordan': 1}
+        }
+      }
+    };
+    const nextState = reducer(undefined, action);
+
+    expect(nextState).to.equal(fromJS({
+      vote: {
+        pair: ['Jordan', 'Costa Rica'],
+        tally: {'Jordan': 1}
+      }
+    }));
+  });
+
+});
