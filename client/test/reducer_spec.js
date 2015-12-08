ac3a1b23119f9a3af226fc5468d6cc1b041c0cf2
chriskohlbrenner <c.kohlbrenner@gmail.com>
2015-12-08 09:17:19 -0500

    wire up client socket io, start dispatching client actions

--- client/test/reducer_spec.js
+++ client/test/reducer_spec.js
@@ -67,4 +67,66 @@ describe('reducer', () => {
     }));
   });
 
+  it('handles VOTE by setting hasVoted', () => {
+    const state = fromJS({
+      vote: {
+        pair: ['Jordan', 'Costa Rica'],
+        tally: {'Jordan': 1}
+      }
+    });
+    const action = {type: 'VOTE', entry: 'Jordan'};
+    const nextState = reducer(state, action);
+
+    expect(nextState).to.equal(fromJS({
+      vote: {
+        pair: ['Jordan', 'Costa Rica'],
+        tally: {'Jordan': 1}
+      },
+      hasVoted: 'Jordan'
+    }));
+  });
+
+  it('does not set hasVoted for VOTE on invalid entry', () => {
+    const state = fromJS({
+      vote: {
+        pair: ['Jordan', 'Costa Rica'],
+        tally: {'Jordan': 1}
+      }
+    });
+    const action = {type: 'VOTE', entry: 'Germany'};
+    const nextState = reducer(state, action);
+
+    expect(nextState).to.equal(fromJS({
+      vote: {
+        pair: ['Jordan', 'Costa Rica'],
+        tally: {'Jordan': 1}
+      }
+    }));
+  });
+
+  it('removes hasVoted on SET_STATE if pair changes', () => {
+    const initialState = fromJS({
+      vote: {
+        pair: ['Jordan', 'Costa Rica'],
+        tally: {'Jordan': 1}
+      },
+      hasVoted: 'Jordan'
+    });
+    const action = {
+      type: 'SET_STATE',
+      state: {
+        vote: {
+          pair: ['Germany', 'Spain']
+        }
+      }
+    };
+    const nextState = reducer(initialState, action);
+
+    expect(nextState).to.equal(fromJS({
+      vote: {
+        pair: ['Germany', 'Spain']
+      }
+    }));
+  });
+
 });
